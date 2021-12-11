1575864bef5910b96bb09456d6ab5e0eb6416277
Andrew Mackrodt <andrewmackrodt@gmail.com>
2021-12-11 04:10:24 +0000

    Optimize 2021 day 7

--- src/2021/07/index.ts
+++ src/2021/07/index.ts
@@ -2,80 +2,51 @@ interface Options {
     burn?: 'constant' | 'linear'
 }
 
-class FuelCalculator {
-    private readonly positions: Record<number, number> = {}
-    private min = Number.MAX_SAFE_INTEGER
-    private max = 0
-
-    public static createFromString(input: string): FuelCalculator {
-        const instance = new FuelCalculator()
-        input.split(',').map(s => {
-            const n = parseInt(s, 10)
-            if (n < instance.min) instance.min = n
-            if (instance.max < n) instance.max = n
-            if ( ! (n in instance.positions)) instance.positions[n] = 0
-            instance.positions[n]++
-        })
-        return instance
-    }
-
-    public getMinimumUsage(options?: Options): number {
-        if ( ! options?.burn) console.warn('calling getMinimumUsage without specifying options.burn is deprecated')
-
-        let left = this.min
-        let right = this.max
-
-        for (let diff = right - left; diff > 0; diff = right - left) {
-            const midIndex = (left + right) / 2
-
-            if (midIndex % 2 === 0) {
-                const midLeftIndex = midIndex - 1
-                const midRightIndex = midIndex
-                // move left
-                if (this.getUsage(midLeftIndex, options) < this.getUsage(midRightIndex, options)) {
-                    right = midLeftIndex + 1
-                }
-                // move right
-                else {
-                    left = midRightIndex
-                }
-            } else {
-                // move left
-                if (this.getUsage(left, options) < this.getUsage(right, options)) {
-                    right = Math.floor(midIndex)
-                }
-                // move right
-                else {
-                    left = Math.ceil(midIndex)
-                }
-            }
+const calc = {
+    median: (values: number[]): number => {
+        const sorted = values.sort((a, b) => a - b)
+        if (sorted.length % 2 === 0) {
+            const rightIndex = sorted.length / 2
+            const sum = sorted[rightIndex - 1] + sorted[rightIndex]
+            return sum / 2
+        } else {
+            return sorted[Math.floor(sorted.length / 2)]
         }
-
-        return this.getUsage(left, options)
-    }
-
-    private getUsage(target: number, options?: Options) {
-        let moves = 0
-        for (const key in this.positions) {
-            const position = parseInt(key, 10)
-            let diff = Math.abs(target - position)
-            if (options?.burn === 'linear') {
-                for (let i = 1, len = diff; i < len; i++) {
-                    diff += i
-                }
-            }
-            if (diff > 0) {
-                moves += diff * this.positions[key]
-            }
-        }
-        return moves
-    }
+    },
+    sum: (values: number[]) => {
+        return values.reduce((res, cur) => res + cur, 0)
+    },
 }
 
 export function solve(input: string, options?: Options): number {
-    const fuelCalculator = FuelCalculator.createFromString(input)
+    const positions = input.split(',').map(s => parseInt(s, 10)).sort((a, b) => a - b)
 
-    return fuelCalculator.getMinimumUsage(options)
+    const totalCost = (cost: (startingPosition: number) => number): number => {
+        return calc.sum(positions.map(p => cost(p)))
+    }
+
+    switch (options?.burn) {
+        case 'linear':
+            const cost = (startingPosition: number, endingPosition: number) => {
+                let change = Math.abs(startingPosition - endingPosition)
+                for (let i = 1, len = change; i < len; i++) {
+                    change += i
+                }
+                return change
+            }
+
+            const mean = calc.sum(positions) / positions.length
+            if (mean % 2 !== 0) {
+                const min = totalCost((p) => cost(p, Math.floor(mean)))
+                const max = totalCost((p) => cost(p, Math.ceil(mean)))
+                return Math.min(min, max)
+            } else {
+                return totalCost((p) => cost(p, mean))
+            }
+        default:
+            const median = calc.median(positions)
+            return totalCost(p => Math.abs(p - median))
+    }
 }
 
 export const partOne = (input: string) => solve(input)
