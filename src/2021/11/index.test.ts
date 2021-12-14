031496eb4c7b763d019ceead8562647982c9d8ac
Andrew Mackrodt <andrewmackrodt@gmail.com>
2021-12-14 21:26:46 +0000

    Add 2021 day 11 part 1

---
+++ src/2021/11/index.test.ts
@@ -0,0 +1,18 @@
+import { solve } from './index.js'
+
+describe('Day 11', () => {
+    const input = `5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526`
+
+    it('returns the number flashes after 100 steps', () => {
+        expect(solve(input, 100)).toEqual(1656)
+    })
+})
---
+++ src/2021/11/index.ts
@@ -0,0 +1,81 @@
+function getAdjacent(map: number[][], x: number, y: number): [number, number][] {
+    const isCheckTop = y > 0
+    const isCheckBottom = y < map.length - 1
+    const isCheckRight = x < map[y].length - 1
+    const isCheckLeft = x > 0
+    const adjacent: [number, number][] = []
+
+    if (isCheckTop) {
+        adjacent.push([x, y - 1])
+        if (isCheckRight) {
+            adjacent.push([x + 1, y - 1])
+        }
+    }
+
+    if (isCheckRight) {
+        adjacent.push([x + 1, y])
+    }
+
+    if (isCheckBottom) {
+        if (isCheckRight) adjacent.push([x + 1, y + 1])
+        adjacent.push([x, y + 1])
+        if (isCheckLeft) adjacent.push([x - 1, y + 1])
+    }
+    if (isCheckLeft) {
+        adjacent.push([x - 1, y])
+        if (isCheckTop) {
+            adjacent.push([x - 1, y - 1])
+        }
+    }
+
+    return adjacent
+}
+
+function step(map: number[][]): number {
+    let total = 0
+
+    const forEach = (cb: (x: number, y: number) => void) => {
+        for (let y = 0; y < map.length; y++) {
+            for (let x = 0; x < map[y].length; x++) {
+                cb(x, y)
+            }
+        }
+    }
+
+    const increment = (x: number, y: number) => {
+        // return if already flashed this step
+        if (map[y][x] > 9) {
+            return
+        }
+        // increment energy
+        map[y][x]++
+        // flash if energy is greater than 9
+        if (map[y][x] > 9) {
+            // increment total
+            total++
+            // increment adjacent peers
+            for (const [ax, ay] of getAdjacent(map, x, y)) {
+                increment(ax, ay)
+            }
+        }
+    }
+
+    forEach(increment)
+
+    forEach((x, y) => {
+        if (map[y][x] > 9) {
+            map[y][x] = 0
+        }
+    })
+
+    return total
+}
+
+export function solve(input: string, steps: number): number {
+    const map = input.split('\n').map(line => line.split('').map(char => parseInt(char, 10)))
+    let flashes = 0
+    for (let i = 0; i < steps; i++) {
+        flashes += step(map)
+    }
+    return flashes
+}
