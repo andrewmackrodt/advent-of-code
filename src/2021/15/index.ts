e240bc0453496c125f1fb449aeabd9a084e50b78
Andrew Mackrodt <andrewmackrodt@gmail.com>
2021-12-17 17:27:32 +0000

    Optimize 2021 day 15

--- src/2021/15/index.ts
+++ src/2021/15/index.ts
@@ -1,47 +1,70 @@
-const sum = (values: number[]) => values.reduce((sum, n) => sum + n, 0)
+import { PriorityQueue } from '../../utils/PriorityQueue.js'
 
-export function solve(input: string, multiply = 1): number {
-    const cavern = input.split('\n').map(s => s.split('').map(c => parseInt(c, 10)))
+export function solve(input: string, multiplier = 1): number {
+    const map = input.split('\n').map(s => s.split('').map(c => parseInt(c, 10)))
 
     // extend right
-    for (let y = 0, h = cavern.length; y < h; y++) {
-        const width = cavern[y].length
-        for (let i = 1; i < multiply; i++) {
-            cavern[y] = [
-                ...cavern[y],
-                ...cavern[y].slice(0, width)
+    for (let y = 0, h = map.length; y < h; y++) {
+        const width = map[y].length
+        for (let i = 1; i < multiplier; i++) {
+            map[y] = [
+                ...map[y],
+                ...map[y].slice(0, width)
                     .map(n => n + i < 10 ? n + i : (n + i) % 9)]
         }
     }
 
     // extend down
-    for (let i = 1, h = cavern.length; i < multiply; i++) {
+    for (let i = 1, h = map.length; i < multiplier; i++) {
         for (let y = 0; y < h; y++) {
-            cavern.push(cavern[y].map(n => n + i < 10 ? n + i : (n + i) % 9))
+            map.push(map[y].map(n => n + i < 10 ? n + i : (n + i) % 9))
         }
     }
 
-    const width = cavern[0].length
-    const height = cavern.length
+    const height = map.length
+    const width  = map[0].length
+    const start = `${0},${0}`
+    const end = [width - 1, height - 1].join(',')
+    const graph: Record<string, Record<string, number>> = {}
 
-    const visited: Record<string, number[]> = {}
-
-    const traverse = (x = 0, y = 0): number[] => {
-        const path = [cavern[y][x]]
-        const key = `${x},${y}`
-        if (key in visited) return [...path, ...visited[key]]
-        const neighbours: [number, number][] = []
-        if (x < width - 1) neighbours.push([x + 1, y])
-        if (y < height - 1) neighbours.push([x, y + 1])
-        if (neighbours.length === 0) return path
-        const res = neighbours.map(n => traverse(n[0], n[1])).sort((a, b) => sum(a) - sum(b))[0]
-        visited[key] = res
-        return [...path, ...res]
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            const vertex = `${x},${y}`
+            const neighbours = []
+            if ( y > 0 )           neighbours.push([x, y - 1])
+            if ( x < width - 1 )   neighbours.push([x + 1, y])
+            if ( y < height - 1 )  neighbours.push([x, y + 1])
+            if ( x > 0 )           neighbours.push([x - 1, y])
+            graph[vertex] = {}
+            neighbours.forEach(([x2, y2]) => graph[vertex][`${x2},${y2}`] = map[y2][x2])
+        }
     }
 
-    const shortest = traverse()
+    const risk: Record<string, number> = {}
+    Object.keys(graph).forEach(xy => risk[xy] = Number.MAX_VALUE)
+    risk[start] = 0
 
-    return sum(shortest.slice(1))
+    const queue = new PriorityQueue<[string, number]>((a, b) => a[1] < b[1])
+    queue.push([start, risk[start]])
+
+    const visited = new Set()
+
+    for (const [xy] of queue) {
+        if (visited.has(xy)) {
+            continue
+        }
+
+        visited.add(xy)
+
+        for (const neighbour in graph[xy]) {
+            if (risk[xy] + graph[xy][neighbour] < risk[neighbour]) {
+                risk[neighbour] = risk[xy] + graph[xy][neighbour]
+                queue.push([neighbour, risk[neighbour]])
+            }
+        }
+    }
+
+    return risk[end]
 }
 
 export const partOne = (input: string) => solve(input)
