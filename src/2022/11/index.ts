c212584d7dfabf5dced3dda421b6e47fe78a012d
Andrew Mackrodt <andrewmackrodt@gmail.com>
2022-12-11 05:44:08 +0000

    Add 2022 day 11 part 1

---
+++ src/2022/11/index.ts
@@ -0,0 +1,94 @@
+interface Monkey {
+    items: number[]
+    operation: (item: number) => number
+    test: {
+        expr: (item: number) => boolean
+        true: number
+        false: number
+    }
+}
+
+const monkeyRegExp = new RegExp(/^Monkey ([0-9]+):$/)
+const itemsRegExp = new RegExp(/^Starting items: ([0-9]+(?:, ?[0-9]+)*)$/)
+const operationRegExp = new RegExp(/^Operation: new = (old|[0-9]+) ?([*+]) ?(old|[0-9]+)$/)
+const testRegExp = new RegExp(/^Test: divisible by ([0-9]+)$/)
+const ifTrueRegExp = new RegExp(/^If true: throw to monkey ([0-9]+)$/)
+const ifFalseRegExp = new RegExp(/^If false: throw to monkey ([0-9]+)$/)
+
+function parseInput(input: string): Monkey[] {
+    const behaviours: Monkey[] = []
+    let behaviour: Monkey
+
+    input.trim().replaceAll(/^[ \t]+|[ \t]+$/gm, '')
+        .split('\n')
+        .filter(line => Boolean(line.length))
+        .forEach((line) => {
+            let match: RegExpExecArray | null
+            switch (true) {
+                case (match = monkeyRegExp.exec(line)) !== null:
+                    behaviour = {
+                        items: [],
+                        operation: (item: number) => item,
+                        test: {
+                            expr: (item: number) => false,
+                            true: -1,
+                            false: -1,
+                        },
+                    }
+                    behaviours[parseInt(match![1], 10)] = behaviour
+                    break
+                case (match = itemsRegExp.exec(line)) !== null:
+                    behaviour.items = match![1].split(/, ?/).map(s => parseInt(s, 10))
+                    break
+                case (match = operationRegExp.exec(line)) !== null:
+                    const [opA, symbol, opB] = match!.slice(1)
+                    behaviour.operation = item => {
+                        const valA = opA === 'old' ? item : parseInt(opA, 10)
+                        const valB = opB === 'old' ? item : parseInt(opB, 10)
+                        switch (symbol) {
+                            case '*':
+                                return valA * valB
+                            case '+':
+                                return valA + valB
+                            default:
+                                throw new Error(`Parse error: ${line}`)
+                        }
+                    }
+                    break
+                case (match = testRegExp.exec(line)) !== null:
+                    behaviour.test.expr = (item: number) => item % parseInt(match![1], 10) === 0
+                    break
+                case (match = ifTrueRegExp.exec(line)) !== null:
+                    behaviour.test.true = parseInt(match![1], 10)
+                    break
+                case (match = ifFalseRegExp.exec(line)) !== null:
+                    behaviour.test.false = parseInt(match![1], 10)
+                    break
+                default:
+                    throw new Error(`Parse error: ${line}`)
+            }
+        })
+
+    return behaviours
+}
+
+export function partOne(input: string): number {
+    const behaviours = parseInput(input)
+    const counts = new Array(behaviours.length).fill(0)
+    for (let round = 0; round < 20; round++) {
+        for (const monkey in behaviours) {
+            const behaviour = behaviours[monkey]
+            while (behaviour.items.length > 0) {
+                let worry = behaviour.items.shift()!
+                worry = behaviour.operation(worry)
+                worry = Math.floor(worry / 3)
+                counts[monkey]++
+                const nextMonkey = behaviour.test.expr(worry)
+                    ? behaviour.test.true
+                    : behaviour.test.false
+                behaviours[nextMonkey].items.push(worry)
+            }
+        }
+    }
+    return counts.sort((a, b) => b - a).slice(0, 2).reduce((total, v) => total * v, 1)
+}
