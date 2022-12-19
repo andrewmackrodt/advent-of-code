e40f6c2adfd50208da24f22513cf6c8f4068cfda
Andrew Mackrodt <andrewmackrodt@gmail.com>
2022-12-19 07:48:26 +0000

    Add 2022 day 18 part 1

---
+++ src/2022/18/index.test.ts
@@ -0,0 +1,21 @@
+import { partOne } from './index.js'
+
+const input = `2,2,2
+1,2,2
+3,2,2
+2,1,2
+2,3,2
+2,2,1
+2,2,3
+2,2,4
+2,2,6
+1,2,5
+3,2,5
+2,1,5
+2,3,5`
+
+describe('partOne', () => {
+    it('returns part one answer', () => {
+        expect(partOne(input)).toEqual(64)
+    })
+})
---
+++ src/2022/18/index.ts
@@ -0,0 +1,37 @@
+interface Point3D {
+    x: number
+    y: number
+    z: number
+}
+
+function parseInput(input: string): Point3D[] {
+    return input.split('\n')
+        .map(line => line.split(',').map(n => parseInt(n, 10)))
+        .reduce((points, split) => {
+            if (split.length === 3 && split.filter(n => ! isNaN(n))) {
+                points.push({ x: split[0], y: split[1], z: split[2] })
+            }
+            return points
+        }, [] as Point3D[])
+}
+
+export function partOne(input: string): number {
+    const points = parseInput(input)
+    let sides = points.length * 6
+    for (let i = 0; i < points.length; i++) {
+        const a = points[i]
+        for (let j = 0; j < points.length; j++) {
+            if (j === i) {
+                continue
+            }
+            const b = points[j]
+            if (Math.abs(a.x - b.x) +
+                Math.abs(a.y - b.y) +
+                Math.abs(a.z - b.z) === 1
+            ) {
+                sides--
+            }
+        }
+    }
+    return sides
+}
