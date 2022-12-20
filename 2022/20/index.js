e2bbb4507e25fa7a66f11fa2aeb39d1ebfb1480f
andrewmackrodt <andrewmackrodt@users.noreply.github.com>
2022-12-20 19:19:03 +0000

    deploy: 88a44a42035d603ccb0462dbcc6bb8bf198c6b31

--- 2022/20/index.js
+++ 2022/20/index.js
@@ -2,30 +2,34 @@ function parseInput(input) {
     return input.split('\n')
         .map(line => parseInt(line, 10))
         .filter(n => !isNaN(n))
-        .map(value => ({ value, moved: false }));
+        .map((value, index) => ({ value, index }));
 }
-function parseSequence(input) {
+function parseSequence(input, rounds, multi) {
     let items = parseInput(input);
-    for (let i = 0, len = items.length; i < len; i++) {
-        const idx = items.findIndex(s => !s.moved);
-        const item = items[idx];
-        item.moved = true;
-        if (item.value === 0) {
-            continue;
+    items.forEach(i => i.value *= multi);
+    for (let r = 0; r < rounds; r++) {
+        for (let i = 0, len = items.length; i < len; i++) {
+            const idx = items.findIndex(s => s.index === i);
+            const item = items[idx];
+            if (item.value === 0) {
+                continue;
+            }
+            items.splice(idx, 1);
+            let newIdx = (idx + item.value) % (len - 1);
+            if (newIdx === 0) {
+                newIdx = items.length;
+            }
+            items = items.slice(0, newIdx).concat(item).concat(...items.slice(newIdx));
         }
-        items.splice(idx, 1);
-        let newIdx = (idx + item.value) % (len - 1);
-        if (newIdx === 0) {
-            newIdx = items.length;
-        }
-        items = items.slice(0, newIdx).concat(item).concat(...items.slice(newIdx));
     }
     const sequence = items.map(s => s.value);
     const idx = sequence.findIndex(n => n === 0);
     return [0].concat(sequence.slice(idx + 1)).concat(sequence.slice(0, idx));
 }
-export function partOne(input) {
-    const sequence = parseSequence(input);
+function solve(input, rounds, multi) {
+    const sequence = parseSequence(input, rounds, multi);
     const len = sequence.length;
     return sequence[1000 % len] + sequence[2000 % len] + sequence[3000 % len];
 }
+export const partOne = (input) => solve(input, 1, 1);
+export const partTwo = (input) => solve(input, 10, 811589153);
