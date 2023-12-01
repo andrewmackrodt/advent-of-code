b1d156506f8e2d2a4d8334b8e99f2a5036cfe90f
andrewmackrodt <andrewmackrodt@users.noreply.github.com>
2023-12-01 20:01:03 +0000

    deploy: b7865f50716f99f1a6ab2301295734f8cc22f1f7

--- 2023/01/index.js
+++ 2023/01/index.js
@@ -1,62 +1,33 @@
-export function partOne(input) {
-    const lines = input.trim().replaceAll(/[ \t]+$/gm, '').split('\n');
-    const pairs = [];
-    for (const line of lines) {
-        const pair = [0, 0];
-        for (let i = 0; i < line.length; i++) {
-            if (line[i].match(/[0-9]/)) {
-                const num = parseInt(line[i]);
-                if (pair[0] === 0) {
-                    pair[0] = num;
-                }
-                pair[1] = num;
-            }
-        }
-        pairs.push(pair);
-    }
-    return pairs
-        .map(([a, b]) => parseInt(a.toString() + b.toString()))
-        .reduce((sum, n) => sum + n, 0);
-}
 const numberTextMap = {
-    one: 1,
-    two: 2,
-    three: 3,
-    four: 4,
-    five: 5,
-    six: 6,
-    seven: 7,
-    eight: 8,
-    nine: 9,
+    one: '1',
+    two: '2',
+    three: '3',
+    four: '4',
+    five: '5',
+    six: '6',
+    seven: '7',
+    eight: '8',
+    nine: '9',
 };
-const numberTextRegExp = new RegExp('^(?:' + Object.keys(numberTextMap).join('|') + ')');
-export function partTwo(input) {
+export function partOne(input, matchWords = false) {
     const lines = input.trim().replaceAll(/[ \t]+$/gm, '').split('\n');
     const pairs = [];
+    const matchValues = Object.values(numberTextMap);
+    if (matchWords) {
+        matchValues.push(...Object.keys(numberTextMap));
+    }
+    const regExp = new RegExp('(?=(' + matchValues.map(n => n.toString()).join('|') + '))', 'g');
     for (const line of lines) {
-        const pair = [0, 0];
-        for (let i = 0; i < line.length; i++) {
-            if (line[i].match(/[0-9]/)) {
-                const num = parseInt(line[i]);
-                if (pair[0] === 0) {
-                    pair[0] = num;
-                }
-                pair[1] = num;
+        const pair = ['0', '0'];
+        for (const match of line.matchAll(regExp)) {
+            const num = match[1].length === 1 ? match[1] : numberTextMap[match[1]];
+            if (pair[0] === '0') {
+                pair[0] = num;
             }
-            else {
-                const match = numberTextRegExp.exec(line.substring(i, i + 5));
-                if (match) {
-                    const num = numberTextMap[match[0]];
-                    if (pair[0] === 0) {
-                        pair[0] = num;
-                    }
-                    pair[1] = num;
-                }
-            }
+            pair[1] = num;
         }
         pairs.push(pair);
     }
-    return pairs
-        .map(([a, b]) => parseInt(a.toString() + b.toString()))
-        .reduce((sum, n) => sum + n, 0);
+    return pairs.map(([a, b]) => parseInt(a + b)).reduce((sum, n) => sum + n, 0);
 }
+export const partTwo = (input) => partOne(input, true);
