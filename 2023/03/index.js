6f6571062d6592b822f756c08e1f7c5ce19d25f1
andrewmackrodt <andrewmackrodt@users.noreply.github.com>
2023-12-04 21:18:57 +0000

    deploy: 33194cb7861c16084f20abde3b7bc16c6314c50f

---
+++ 2023/03/index.js
@@ -0,0 +1,67 @@
+export function partOne(input) {
+    const lines = input.trim().split('\n').filter(Boolean);
+    let sum = 0;
+    for (let y = 0; y < lines.length; y++) {
+        const line = lines[y];
+        for (const match of line.matchAll(/\d+/g)) {
+            const x1 = match.index;
+            const x2 = x1 + match[0].length;
+            const ax1 = x1 > 0 ? x1 - 1 : x1;
+            const ax2 = x2 < line.length - 1 ? x2 + 1 : x2;
+            const ay1 = y > 0 ? y - 1 : -1;
+            const ay2 = y < lines.length - 1 ? y + 1 : -1;
+            if (line.substring(ax1, ax2).match(/[^.\d]/) ||
+                (ay1 !== -1 && lines[ay1].substring(ax1, ax2).match(/[^.\d]/)) ||
+                (ay2 !== -1 && lines[ay2].substring(ax1, ax2).match(/[^.\d]/))) {
+                sum += parseInt(match[0]);
+            }
+        }
+    }
+    return sum;
+}
+export function partTwo(input) {
+    const partNumbers = [];
+    const lines = input.trim().split('\n').filter(Boolean);
+    for (let y = 0; y < lines.length; y++) {
+        const line = lines[y];
+        for (const match of line.matchAll(/\d+/g)) {
+            const x1 = match.index;
+            const x2 = x1 + match[0].length - 1;
+            const value = parseInt(match[0]);
+            partNumbers.push({ y, x1, x2, value });
+        }
+    }
+    const addPartNumber = (adjacent, y, x) => {
+        const pn = partNumbers.find(pn => pn.y === y && pn.x1 <= x && x <= pn.x2);
+        if (pn && !adjacent.includes(pn)) {
+            adjacent.push(pn);
+        }
+    };
+    const addPartNumberX = (adjacent, y, x, len) => {
+        if (x < len - 1)
+            addPartNumber(adjacent, y, x + 1); // left
+        if (x > 0)
+            addPartNumber(adjacent, y, x - 1); // right
+    };
+    const addPartNumberY = (adjacent, y, x, len) => {
+        addPartNumber(adjacent, y, x); // top or bottom
+        addPartNumberX(adjacent, y, x, len); // left or right
+    };
+    let sum = 0;
+    for (let y = 0; y < lines.length; y++) {
+        const line = lines[y];
+        for (const match of line.matchAll(/\*/g)) {
+            const adjacent = [];
+            const x = match.index;
+            addPartNumberX(adjacent, y, x, line.length); // left or right
+            if (y > 0)
+                addPartNumberY(adjacent, y - 1, x, line.length); // top
+            if (y < lines.length - 1)
+                addPartNumberY(adjacent, y + 1, x, line.length); // bottom
+            if (adjacent.length === 2) {
+                sum += adjacent[0].value * adjacent[1].value;
+            }
+        }
+    }
+    return sum;
+}
